5ddd6b63818bc9e8685ad70b1ec6d2fc33852942
sanabob <72040706+liewshanhong@users.noreply.github.com>
2023-07-25 16:59:22 -0700

    update 1

---
+++ chat-app/public/js/chat.js
@@ -0,0 +1,25 @@
+const socket = io() 
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
+})
+
+document.querySelector('#share-location').addEventListener('click', () => {
+    if(!navigator.geolocation) return alert('Geolocation is not supported by your browser.')
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        const { latitude, longitude } = position.coords
+        const data = {
+            latitude,
+            longitude
+        }
+
+        socket.emit('sendLocation', data)
+    })
+})
---
+++ chat-app/src/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+let count = 0
+const welcome = 'Welcome to my chatroom!'
+
+io.on('connection', (socket) => {
+    console.log('New web socket connection.')
+
+    socket.broadcast.emit('message', 'A new user has joined the chat.')
+
+    socket.emit('message', welcome)
+
+    socket.on('sendMessage', (message) =>{
+        io.emit('message', message)
+    })
+
+    socket.on('sendLocation', (location) => {
+        io.emit('message', `https://google.com/maps?q=${ location.latitude },${ location.longitude }`)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left.')
+    })
+
+})
+
+server.listen(port, () => {
+    console.log('Server is up on port: ', port)
+})
